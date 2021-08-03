3655ddde1c509d7f6da38d12869a3430d0a5d09a
DJ Drakos <dianajodrakos@gmail.com>
2021-08-02 17:32:52 -0700

    created failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,19 @@ describe('test routes', () => {
   });
 
   it('using POST route, inserts a new entry into database', async () => {
+    const newTest = {
+      test: 'hello world',
+      number: 42,
+      boolean: true,
+    };
 
+    const res = await request(app)
+      .post('/api/v1/test-api')
+      .send(test);
+
+    expect(res.body).toEqual({
+      id: 1,
+      ...newTest,
+    });
   });
 });
